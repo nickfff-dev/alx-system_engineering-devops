b1c7d1bfe7daf5a64ccbc6d53ca0eeceee01adc2
nickfff-dev <hunchographics@gmail.com>
2023-11-25 02:43:39 +0300

    install flask

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# This Puppet manifest installs Flask version 2.1.0 using pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+  user    => 'root',
+}
