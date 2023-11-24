c56b4519fd0a5227d48d396d2a4781ee096c760a
nickfff-dev <hunchographics@gmail.com>
2023-11-25 02:57:21 +0300

    killmenow

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# This Puppet manifest kills a process named 'killmenow' using pkill
+exec { 'kill_killmenow':
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
+  user    => 'root',
+}
