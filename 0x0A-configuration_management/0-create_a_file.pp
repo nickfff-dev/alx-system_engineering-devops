4ee0dac7dea2a0ab6ce5dce6b5a47ea5fa57b91b
nickfff-dev <hunchographics@gmail.com>
2023-11-25 02:38:36 +0300

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This Puppet manifest creates a file at /tmp/school with specific permissions, owner, group, and content.
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
