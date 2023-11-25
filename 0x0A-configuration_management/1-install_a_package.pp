04208536cbeaf1e5926447a75797e1f726a605c6
nickfff-dev <hunchographics@gmail.com>
2023-11-25 03:07:07 +0300

    puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # This Puppet manifest installs Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin',
-  unless  => '/usr/bin/pip3 --version | grep "Flask==2.1.0"',
-  user    => 'root',
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  source   => 'https://pypi.org/simple/Flask',
 }
