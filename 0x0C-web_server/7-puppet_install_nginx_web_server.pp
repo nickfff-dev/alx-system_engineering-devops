702265803be71b7a1fdb8dd896b2d86c11ebb413
nickfff-dev <hunchographics@gmail.com>
2023-12-03 23:17:25 +0300

    puppet script for nginx cnf

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,8 @@
 # This manifest installs and configures nginx
-class nginx_setup {
+
+  package { 'nginx':
+  ensure => 'present',
+  }
 
   exec { 'apt-update':
     command  => 'sudo /usr/bin/apt-get update',
@@ -19,7 +22,8 @@ class nginx_setup {
   }
 
   exec { 'add-redirect':
-    command  => 'sudo sed -i "s/server_name _;/server_name _;\\n\\trewrite ^\/redirect_me https:\/\/stackoverflow.com permanent;/" /etc/nginx/sites-available/default',
+    command  => 'sudo sed -i "s/server_name _;/server_name _;\\n\\trewrite ^\/redirect_me https:\/\/stackoverflow.com permanent;/"
+    /etc/nginx/sites-available/default',
     provider => shell,
     require  => Exec['create-index'],
   }
@@ -31,7 +35,8 @@ class nginx_setup {
   }
 
   exec { 'configure-nginx':
-    command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\terror_page 404 \/404.html;\\n\\tlocation = \/404.html {\\n\\t\\troot \/var\/www\/html;\\n\\t}/" /etc/nginx/sites-available/default',
+    command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\terror_page 404 \/404.html;\\n\\tlocation =
+    \/404.html {\\n\\t\\troot \/var\/www\/html;\\n\\t}/" /etc/nginx/sites-available/default',
     provider => shell,
     require  => Exec['create-404'],
   }
@@ -41,4 +46,3 @@ class nginx_setup {
     provider => shell,
     require  => Exec['configure-nginx'],
   }
-}
