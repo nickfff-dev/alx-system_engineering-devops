653e31fa32b6c258ecbfa67e50b7a6571e04f4d3
nickfff-dev <hunchographics@gmail.com>
2023-12-03 22:56:18 +0300

    puppet script for nginx cnf

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,43 @@
+class nginx_setup {
+
+  exec { 'apt-update':
+    command  => 'sudo /usr/bin/apt-get update',
+    provider => shell,
+  }
+
+  exec { 'install-nginx':
+    command  => 'sudo /usr/bin/apt-get install -y nginx',
+    provider => shell,
+    require  => Exec['apt-update'],
+  }
+
+  exec { 'create-index':
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    provider => shell,
+    require  => Exec['install-nginx'],
+  }
+
+  exec { 'add-redirect':
+    command  => 'sudo sed -i "s/server_name _;/server_name _;\\n\\trewrite ^\/redirect_me https:\/\/stackoverflow.com permanent;/" /etc/nginx/sites-available/default',
+    provider => shell,
+    require  => Exec['create-index'],
+  }
+
+  exec { 'create-404':
+    command  => 'echo "Ceci n\'est pas une page" | sudo tee /var/www/html/404.html',
+    provider => shell,
+    require  => Exec['add-redirect'],
+  }
+
+  exec { 'configure-nginx':
+    command  => 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\terror_page 404 \/404.html;\\n\\tlocation = \/404.html {\\n\\t\\troot \/var\/www\/html;\\n\\t}/" /etc/nginx/sites-available/default',
+    provider => shell,
+    require  => Exec['create-404'],
+  }
+
+  exec { 'restart-nginx':
+    command  => 'sudo service nginx restart',
+    provider => shell,
+    require  => Exec['configure-nginx'],
+  }
+}
