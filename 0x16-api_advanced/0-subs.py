4b0183e45f08f959cabf92e2e8d9a8eecc48d328
nickfff-dev <hunchographics@gmail.com>
2024-01-16 23:57:52 +0300

    subs unknown error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 """
-Contains the number_of_subscribers function
+Contain function that queries the Reddit API and returns sub
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """Return the number of subscribers"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/70.0.3538.77 Safari/537.36'}
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v0.0.0 (by /u/qendev)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        subname = data.get('data', {}).get('display_name', None)
+        if subname is None or subname.lower() != subreddit.lower():
+            return 0
+        return data.get('data', {}).get('subscribers', 0)
