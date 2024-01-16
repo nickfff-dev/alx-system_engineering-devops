ef8ffc75ed027a495f66c5ac3682a2bad3e52d1a
nickfff-dev <hunchographics@gmail.com>
2024-01-17 00:07:26 +0300

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Contain function that queries the Reddit API"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the top 10 hot posts for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko)\
+                Chrome/70.0.3538.77 Safari/537.36'}
+    if subreddit is None or type(subreddit) is not str:
+        print(None)
+        return
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        if 'error' in data:
+            print(None)
+            return
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
