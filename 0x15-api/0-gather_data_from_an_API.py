faeffaa6273ab54e28053f6a4fe53acdf5553073
nickfff-dev <hunchographics@gmail.com>
2024-03-26 07:20:12 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,14 @@
 #!/usr/bin/python3
-"""Export data to JSON format"""
-
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    """Export data to JSON format"""
-    user_id = sys.argv[1]
-    if user_id.isdigit() is True:
-        user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-        todos_url = f"{user_url}/todos"
-        tasks = []
-        completed_tasks = 0
-        total_tasks = 0
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-        todos_res = requests.get(todos_url)
-        todos = todos_res.json()
-        total_tasks = len(todos)
-
-        user_res = requests.get(user_url)
-        user = user_res.json()
-        name = user.get("name")
-        username = user.get("username")
-
-        [
-            tasks.append(todo["title"]) for todo in todos
-            if todo["completed"] is True
-
-        ]
-        completed_tasks = len(tasks)
-
-        # Print to stdout
-        print(f"Employee {name} is done with tasks"
-              f"({completed_tasks}/{total_tasks}):")
-        for task in tasks:
-            print(f"\t {task}")
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,67 +1,19 @@
 #!/usr/bin/python3
-"""Export data to JSON format"""
-
-
-import csv
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    """Export data to JSON format"""
     user_id = sys.argv[1]
-    if user_id.isdigit() is True:
-        user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-        todos_url = f"{user_url}/todos"
-        tasks = []
-        completed_tasks = 0
-        total_tasks = 0
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-        todos_res = requests.get(todos_url)
-        todos = todos_res.json()
-        total_tasks = len(todos)
-
-        user_res = requests.get(user_url)
-        user = user_res.json()
-
-        name = user.get("name")
-        username = user.get("username")
-
-        [
-            tasks.append(todo["title"]) for todo in todos
-            if todo["completed"] is True
-
-        ]
-        completed_tasks = len(tasks)
-
-        # Print to stdout
-        print(f"Employee {name} is done with tasks"
-              f"({completed_tasks}/{total_tasks}):")
-        for task in tasks:
-            print(f"\t {task}")
-
-        # Write to CSV
-        with open(f'{user_id}.csv', 'w', newline='') as csvfile:
-            fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
-                          'TASK_TITLE']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                    quoting=csv.QUOTE_ALL)
-            [
-                writer.writerow({
-                    'USER_ID': str(todo.get('userId')),
-                    'USERNAME': username,
-                    'TASK_COMPLETED_STATUS': str(todo.get('completed')),
-                    'TASK_TITLE': todo.get('title')
-                }) for todo in todos
-            ]
-
-        # Write to JSON
-        with open(f"{user_id}.json", "w") as jsonfile:
-            json.dump({
-                user_id: [{
-                    "task": todo.get("title"),
-                    "completed": todo.get("completed"),
-                    "username": username
-                } for todo in todos]
-            }, jsonfile)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
